19d8a5dd21c16c3cfbed26fb3b9f474be3b1191f
Fedi-Said-Ghourabi <47000166+Fedi-Said-Ghourabi@users.noreply.github.com>
2021-07-12 18:42:42 +0100

    Added Product & Category Manager in Back Office

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,82 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/admin/product/add", name="add_product")
      */
-    public function index(): Response
+    public function add(Request $request): Response
     {
-        return $this->render('product/add.html.twig', [
+        $prod = new Product();
+        $form = $this->createForm(ProductType::class, $prod);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($prod);
+            $em->flush();
+            return $this->redirectToRoute('list_product');
+        }
+        return $this->render('BackOffice/product/add.html.twig', [
             'controller_name' => 'ProductController',
+            "form_title" => "Add Product",
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route ("admin/product/list", name="list_product")
+     */
+    public function list()
+    {
+        $prod = $this->getDoctrine()->getRepository(Product::class)->findAll();
+        return $this->render("BackOffice/product/list.html.twig", [
+            "product" => $prod,
+        ]);
+    }
+
+    /**
+     * @Route("admin/product/delete/{id}", name="delete_product")
+     */
+    public function delete(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $prod = $entityManager->getRepository(Product::class)->find($id);
+        $entityManager->remove($prod);
+        $entityManager->flush();
+        return $this->redirectToRoute('list_product');
+    }
+
+    /**
+     * @Route("admin/product/edit/{id}", name="edit_product")
+     */
+    public function edit(Request $request, int $id): Response
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $prod = $entityManager->getRepository(Product::class)->find($id);
+        $form = $this->createForm(ProductType::class, $prod);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('list_product');
+
+        }
+        return $this->render("BackOffice/product/edit.html.twig", [
+            "form_title" => "Modifier vos informations",
+            "form" => $form->createView(),
         ]);
     }
 }
